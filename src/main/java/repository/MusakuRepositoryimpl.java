3ace0f60cbb3ea784dd7fef95c4978504fa55a4a
inali <pferdian147@gmail.com>
2025-04-05 13:59:13 +0700

    melengkapi file  repoIMPL

--- src/main/java/repository/MusakuRepositoryimpl.java
+++ src/main/java/repository/MusakuRepositoryimpl.java
@@ -1,5 +1,21 @@
 package repository;
 
-public class MusakuRepositoryimpl {
+import entity.Musaku;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MusakuRepositoryimpl implements MusakuRepository{
+    private ArrayList<String> Musaku = new ArrayList<>();
+
+
+    @Override
+    public List<entity.Musaku> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public void add(entity.Musaku musaku) {
+
+    }
 }
